d20235b6ec67c09c1470b7cd9b8293cf8d9460aa
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-06-26 18:11:31 +0530

    feat: Fetch User Data Function

--- popup/script.js
+++ popup/script.js
@@ -22,4 +22,17 @@ document.addEventListener('DOMContentLoaded', function () {
       timeout: 3000,
     });
   }
+
+  // Function to fetch user data from the API
+  async function fetchUserData(username) {
+    const apiUrl = `https://leetcode-stats-api.herokuapp.com/${username}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      showErrorToast(error);
+    }
+  }
 });
