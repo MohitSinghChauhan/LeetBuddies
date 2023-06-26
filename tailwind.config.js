7e7e1bac7205e96280aa1c8f7830af88863e62d0
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-06-26 17:54:08 +0530

    fix: tailwind config added for proper functioning

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // Other Tailwind CSS configuration options...
+  
+    // Specify the files to scan for utility classes
+    content: [
+      './popup/*.html',
+      './popup/*.js',
+      // Add more file patterns if necessary
+    ],
+  };
+  
