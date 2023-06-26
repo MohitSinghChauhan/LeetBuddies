37fa12ed9c553d37027b5e5ab885fafe9475a9fb
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-06-26 18:12:58 +0530

    feat: Render Friend Data Function

--- popup/script.js
+++ popup/script.js
@@ -35,4 +35,78 @@ document.addEventListener('DOMContentLoaded', function () {
       showErrorToast(error);
     }
   }
+
+  // Function to render friend's data on the popup page
+  function renderFriendData(friend, index) {
+    const friendItem = document.createElement('div');
+    friendItem.classList.add(
+      'friend',
+      'flex',
+      'items-center',
+      'justify-between',
+      'bg-white',
+      'rounded',
+      'p-2',
+      'mb-2'
+    );
+
+    const friendName = document.createElement('a');
+    friendName.textContent = friend.username;
+    friendName.href = `https://leetcode.com/${friend.username}`;
+    friendName.target = '_blank';
+    friendName.classList.add('text-blue-500', 'font-medium');
+    friendItem.appendChild(friendName);
+
+    const hoverModal = document.createElement('div');
+    hoverModal.classList.add('box');
+
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://leetcard.jacoblin.cool/${friend.username}?theme=unicorn&font=Fira%20Code&ext=heatmap`;
+    iframe.width = '400px';
+    iframe.height = '100%';
+    iframe.style.border = 'none';
+    hoverModal.appendChild(iframe);
+
+    friendItem.appendChild(hoverModal);
+
+    const detailsContainer = document.createElement('div');
+    detailsContainer.classList.add('flex', 'gap-4');
+
+    const easyCount = document.createElement('span');
+    easyCount.textContent = `Easy: ${friend.easySolved}/${friend.totalEasy}`;
+    easyCount.classList.add('text-green-500', 'font-medium');
+    detailsContainer.appendChild(easyCount);
+
+    const mediumCount = document.createElement('span');
+    mediumCount.textContent = `Medium: ${friend.mediumSolved}/${friend.totalMedium}`;
+    mediumCount.classList.add('text-yellow-500', 'font-medium');
+    detailsContainer.appendChild(mediumCount);
+
+    const hardCount = document.createElement('span');
+    hardCount.textContent = `Hard: ${friend.hardSolved}/${friend.totalHard}`;
+    hardCount.classList.add('text-red-500', 'font-medium');
+    detailsContainer.appendChild(hardCount);
+
+    const totalCount = document.createElement('span');
+    totalCount.textContent = `Total: ${friend.totalSolved}/${friend.totalQuestions}`;
+    totalCount.classList.add('text-blue-500', 'font-medium');
+    detailsContainer.appendChild(totalCount);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>'; // FontAwesome trash icon
+    detailsContainer.appendChild(deleteButton);
+
+    // Event listener for delete button
+    deleteButton.addEventListener('click', function () {
+      const friends = JSON.parse(localStorage.getItem('friends')) || [];
+      friends.splice(index, 1);
+      localStorage.setItem('friends', JSON.stringify(friends));
+      renderFriendsList(friends);
+    });
+
+    friendItem.appendChild(detailsContainer);
+
+    return friendItem;
+  }
 });
