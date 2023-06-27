2b873f12d0b7bf44d7d5b2dd5fd869c7a5728a42
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-06-27 15:23:50 +0530

    refactor: Refactor JS code to incorporate best practices and improve readability.

--- popup/script.js
+++ popup/script.js
@@ -1,30 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   const searchInput = document.getElementById('searchInput');
   const friendsList = document.getElementById('friendsList');
   const sortButton = document.getElementById('sortButton');
 
   // Function to show an error toast notification
-  function showErrorToast(message) {
+  const showErrorToast = (message) => {
     iziToast.error({
       title: 'Error',
       message: message,
       position: 'topRight',
       timeout: 3000,
     });
-  }
+  };
 
   // Function to show a success toast notification
-  function showSuccessToast(message) {
+  const showSuccessToast = (message) => {
     iziToast.success({
       title: 'Success',
       message: message,
       position: 'topRight',
       timeout: 3000,
     });
-  }
+  };
 
   // Function to fetch user data from the API
-  async function fetchUserData(username) {
+  const fetchUserData = async (username) => {
     const apiUrl = `https://leetcode-stats-api.herokuapp.com/${username}`;
 
     try {
@@ -34,10 +34,10 @@ document.addEventListener('DOMContentLoaded', function () {
     } catch (error) {
       showErrorToast(error);
     }
-  }
+  };
 
   // Function to render friend's data on the popup page
-  function renderFriendData(friend, index) {
+  const renderFriendData = (friend, index) => {
     const friendItem = document.createElement('div');
     friendItem.classList.add(
       'friend',
@@ -98,7 +98,7 @@ document.addEventListener('DOMContentLoaded', function () {
     detailsContainer.appendChild(deleteButton);
 
     // Event listener for delete button
-    deleteButton.addEventListener('click', function () {
+    deleteButton.addEventListener('click', () => {
       const friends = JSON.parse(localStorage.getItem('friends')) || [];
       friends.splice(index, 1);
       localStorage.setItem('friends', JSON.stringify(friends));
@@ -108,67 +108,64 @@ document.addEventListener('DOMContentLoaded', function () {
     friendItem.appendChild(detailsContainer);
 
     return friendItem;
-  }
+  };
 
   // Function to render friends list
-  function renderFriendsList(friends) {
+  const renderFriendsList = (friends) => {
     friendsList.innerHTML = '';
 
     friends.forEach((friend, index) => {
       const friendItem = renderFriendData(friend, index);
       friendsList.appendChild(friendItem);
     });
-  }
+  };
+
+  // Function to sort friends by total problems solved
+  const sortFriendsByTotalProblems = (friends) => {
+    return friends.sort((a, b) => b.totalSolved - a.totalSolved);
+  };
 
   // Event listener for sort button
-  sortButton.addEventListener('click', function () {
+  sortButton.addEventListener('click', () => {
     const friends = JSON.parse(localStorage.getItem('friends')) || [];
     const sortedFriends = sortFriendsByTotalProblems(friends);
     renderFriendsList(sortedFriends);
-
-    // Update the local storage with the sorted data
     localStorage.setItem('friends', JSON.stringify(sortedFriends));
   });
 
-  // Function to sort friends by total problems solved
-  function sortFriendsByTotalProblems(friends) {
-    return friends.sort((a, b) => b.totalSolved - a.totalSolved);
-  }
-
   // Event listener for search input
-  searchInput.addEventListener('keypress', function (e) {
+  searchInput.addEventListener('keypress', async (e) => {
     if (e.key === 'Enter') {
       const username = searchInput.value.trim();
       if (username !== '') {
-        fetchUserData(username)
-          .then((data) => {
-            if (data.status === 'success') {
-              const friend = {
-                username: username,
-                easySolved: data.easySolved,
-                mediumSolved: data.mediumSolved,
-                hardSolved: data.hardSolved,
-                totalSolved: data.totalSolved,
-                totalEasy: data.totalEasy,
-                totalMedium: data.totalMedium,
-                totalHard: data.totalHard,
-                totalQuestions: data.totalQuestions,
-              };
+        try {
+          const data = await fetchUserData(username);
+          if (data.status === 'success') {
+            const friend = {
+              username: username,
+              easySolved: data.easySolved,
+              mediumSolved: data.mediumSolved,
+              hardSolved: data.hardSolved,
+              totalSolved: data.totalSolved,
+              totalEasy: data.totalEasy,
+              totalMedium: data.totalMedium,
+              totalHard: data.totalHard,
+              totalQuestions: data.totalQuestions,
+            };
 
-              const friends = JSON.parse(localStorage.getItem('friends')) || [];
-              friends.push(friend);
-              localStorage.setItem('friends', JSON.stringify(friends));
+            const friends = JSON.parse(localStorage.getItem('friends')) || [];
+            friends.push(friend);
+            localStorage.setItem('friends', JSON.stringify(friends));
 
-              renderFriendsList(friends);
-              showSuccessToast('User successfully added!');
-            } else {
-              showErrorToast(data.message);
-            }
-          })
-          .catch((error) => {
-            showErrorToast('An error occurred while fetching user data.');
-            console.error(error);
-          });
+            renderFriendsList(friends);
+            showSuccessToast('User successfully added!');
+          } else {
+            showErrorToast(data.message);
+          }
+        } catch (error) {
+          showErrorToast('An error occurred while fetching user data.');
+          console.error(error);
+        }
       }
       searchInput.value = '';
     }
